a9206448e3fa0ed3dd5005a70bc994d83fabf10c
nano442 <oboulo442@gmail.com>
2025-08-22 14:11:19 +0100

    amelioration des fonction de review ,le review doit etre lier au vendeur , pas au produit

--- dist/routes/review.routes.js
+++ dist/routes/review.routes.js
@@ -7,9 +7,13 @@ const express_1 = __importDefault(require("express"));
 const review_controller_js_1 = require("../controllers/review.controller.js");
 const auth_middleware_js_1 = require("../middlewares/auth.middleware.js");
 const router = express_1.default.Router();
+// Routes publiques
 router.get("/", review_controller_js_1.getAllReviews);
+router.get("/seller/:userId", review_controller_js_1.getReviewsForUser);
 router.get("/:id", review_controller_js_1.getReviewById);
+// Routes protégées (authentification requise)
 router.use(auth_middleware_js_1.authenticate);
+router.get("/my-reviews", review_controller_js_1.getReviewsByUser);
 router.post("/", review_controller_js_1.createReview);
 router.put("/:id", review_controller_js_1.updateReview);
 router.delete("/:id", review_controller_js_1.deleteReview);
--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -5,15 +5,21 @@ import {
   getAllReviews,
   getReviewById,
   updateReview,
+  getReviewsForUser,
+  getReviewsByUser,
 } from "../controllers/review.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+// Routes publiques
 router.get("/", getAllReviews);
+router.get("/seller/:userId", getReviewsForUser);
 router.get("/:id", getReviewById);
 
+// Routes protégées (authentification requise)
 router.use(authenticate);
+router.get("/my-reviews", getReviewsByUser);
 router.post("/", createReview);
 router.put("/:id", updateReview);
 router.delete("/:id", deleteReview);
